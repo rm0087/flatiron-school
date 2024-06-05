d616c7f0ba313af6ba798bf429d35b09393db5c8
rm0087 <michetti.ray@gmail.com>
2024-06-05 16:14:30 -0400

    06-05-2024 - post class dump

--- phase-1/events/introduction/beginning/index.js
+++ phase-1/events/introduction/beginning/index.js
@@ -15,15 +15,47 @@ Takeaways
 - `[form].[input].value`
 
 */
-
+const school = {
+    name: "flatiron",
+    cohortSize: 8
+}
 // ~ the "click" event
+document.querySelector("#tomorrow").addEventListener("click", (event)=>{
+    document.querySelector("#forever").textContent = "On June 5, 2024";
+    console.log(event.pointerType);
+});
 
 // ~ the "submit" event
+document.querySelector("form").addEventListener("submit", (event)=>{
+    event.preventDefault();
+    // event === "submit"
+    //event.target === <form>
+    //event.target["date-input"] === <input name="date-input">
+    //event.target["date-input"].value === 2024-06-27
+    console.log(event.target["date-input"].value);
+    document.querySelector("#sometime").textContent= event.target["date-input"].value;
+    console.log("The user submitted a date!");
+});
+
+//console.log(school.name);
+//console.log(school["name"]);
+
 
 // ~ challenges
 // 1. [easy] Add another click event handler.
+document.querySelector("#today").addEventListener("click",(event2)=>{
+    document.querySelector("#today").textContent = "Not today!"
+});
 // 2. [medium] Add some non-click, non-submit event listener to the page.
+document.querySelector("html").addEventListener("wheel", (event3)=>{
+    document.querySelector("#tomorrow").textContent = "Where you going?"
+});
 // 3. [hard] Add an image, and a form with one text input to the page.
 //    On submit, have the image change to the URL specified in the text input.
+document.querySelector("#dates").addEventListener("submit", (event4)=>{
+    const img = document.createElement("img");
+    document.querySelector("img").src = "https://en.wikipedia.org/wiki/Cat#/media/File:Cat_August_2010-4.jpg";
+    document.querySelector("#dates").append(img);
+});
 // 4. [bonus] Add an event handler to #today so that whenever some or all of its text is copied,
 //    that text is set as the content of #sometime.
