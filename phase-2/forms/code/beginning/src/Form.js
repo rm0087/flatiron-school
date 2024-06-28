19a925d94fb78252f55ab1fa9624dc693cb5149e
rm0087 <michetti.ray@gmail.com>
2024-06-28 12:42:43 -0400

    post-class 2

--- phase-2/forms/code/beginning/src/Form.js
+++ phase-2/forms/code/beginning/src/Form.js
@@ -1,3 +1,9 @@
+/*
+1. Implement some input synchronization, e.g. the user's 'lucky number' is the lengths of inputs combined.
+2. Implement some input validation, e.g. the first letter of a user's name must be capitalized.
+3. (BONUS) Rewrite this controlled form where 'name' and 'movie' are managed with one state, e.g. 'formData'.
+
+*/
 import { useState } from "react";
 
 
@@ -5,8 +11,13 @@ export default function Form() {
     const [name, setName] = useState("");
     const [movie, setMovie] = useState("");
 
-    const handleName=(event)=>{setName(event.target.value)}
+    const handleName=(event)=>{setName(event.target.value.toUpperCase().charAt(0) + event.target.value.slice(1))}
+    
     const handleMovie=(event)=>{setMovie(event.target.value)}
+
+    const sum = movie.length+name.length;
+
+
     
     return (
         <form>
@@ -18,7 +29,7 @@ export default function Form() {
                 <input value={movie} onChange={handleMovie}/>
             </label>
             <br/><br/>
-            <em>Your lucky number is</em> <strong>2</strong>!
+            <em>Your lucky number is</em> <strong>{sum}</strong>!
             <br/><br/>
             <input type="submit" value="Sign Up" onClick/>
             <h2>Members</h2>
